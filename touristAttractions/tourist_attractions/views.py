7017cb1e21a9348126555e019a821e0e2545bfde
KhoaNguyen96 <rin@192-168-1-9.tpgi.com.au>
2022-06-06 12:58:23 +1000

    adding creating function for web app

--- touristAttractions/tourist_attractions/views.py
+++ touristAttractions/tourist_attractions/views.py
@@ -1,3 +1,28 @@
+from email import contentmanager
 from django.shortcuts import render
+from .models import Attraction, State
+from django.views.generic.edit import CreateView
+from .forms import StateCreateForm, AttractionCreateForm
+
 
 # Create your views here.
+def home(request):
+    all_attractions = Attraction.objects.all()
+    context = {"attractions": all_attractions}
+    return render(request, 'tourist_attractions/home.html', context)
+
+def details(request, statename):
+    attractions = Attraction.objects.all()
+    context = {"attractions": attractions, "statename": statename.replace("-", " ")}
+    return render(request, "tourist_attractions/details.html", context)
+
+class StateCreate(CreateView):
+    model = State
+    form_class = StateCreateForm
+    template_name = "tourist_attractions/state_create_form.html"
+
+class AttractionCreate(CreateView):
+    model = Attraction
+    form_class = AttractionCreateForm
+    template_name = "tourist_attractions/attraction_create_form.html"
+
