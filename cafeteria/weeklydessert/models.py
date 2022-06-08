98d6b844c971c29e06275a36f72ea3a28ef3faaa
KhoaNguyen96 <rin@192-168-1-9.tpgi.com.au>
2022-06-08 14:35:52 +1000

    updating

--- cafeteria/weeklydessert/models.py
+++ cafeteria/weeklydessert/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
+class Week(models.Model):
+    pub_date = models.DateTimeField("date published")
+
+    # When creating a new instance, this function inform students the menus of the following week starting on Monday 
+    def start_of_week(self):
+        # Using model 'datetime' to get todays date:
+        today = datetime.date.today()
+        # Subtract the number of date which already passed this week (this gets 'last' Monday).
+        # And add one week
+        start_of_week = today + datetime.timedelta(days=-today.weekday(), weeks=1)
+        return start_of_week
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    week = models.ForeignKey(Week, on_delete=models.CASCADE)
+    dessert_choice = models.CharField(max_length=200)
+    dessert_details = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.dessert_choice
+
+    
--- cafeteria/weeklydessert/views.py
+++ cafeteria/weeklydessert/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+
+from django.shortcuts import redirect, render
+from .models import Week, Choice
+from django.urls import reverse, reverse_lazy
+from django.http import Http404
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin 
 
 # Create your views here.
+
+def index(request):
+    last_week_poll = Week.objects.get(pk=1)
+    context = {
+        "name": request.user,
+        "week_poll": last_week_poll
+    }
+    return render(request, "index.html", context)
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
+
+@login_required
+def logout_request(request):
+    logout(request)
+    return redirect("index")
+
+class DetailsView(LoginRequiredMixin, DetailView):
+    model = Week
+    template_name = "detail.html"
+
+class ResultsView(LoginRequiredMixin, DetailView):
+    model = Week
+    template_name= "results.html"
+
+def vote(request, week_id):
+    try:
+        week = Week.objects.get(pk=week_id)
+        selected_choice = week.choice_set.get(pk=request.POST["choice"])
+    except Week.DoesNotExist:
+        raise Http404("Poll for said week does not exists")
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect("results", week_id)
