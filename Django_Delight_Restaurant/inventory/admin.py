acbb88a2dfd41717062cc1a0b25dbfd4f75314d1
KhoaNguyen96 <rin@192-168-1-9.tpgi.com.au>
2022-06-09 15:38:23 +1000

    update admin.py

--- Django_Delight_Restaurant/inventory/admin.py
+++ Django_Delight_Restaurant/inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Ingredient)
+admin.site.register(models.MenuItem)
+admin.site.register(models.Purchase)
+admin.site.register(models.RecipeRequirement)
+
